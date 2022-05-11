b3c9e5f9a4c5bec55672fd20704cffdf5f48d600
Tapinder Singh <9sin063>
2022-05-11 23:03:35 +0530

    added output

---
+++ terraform/application/output.tf
@@ -0,0 +1,15 @@
+output "base_url" {
+  value = module.APIGateway.base_url
+}
+
+output "api_name" {
+  value = "${var.project}-apigateway-${var.env}"
+}
+
+output "api_arn" {
+  value = module.APIGateway.api_arn
+}
+
+output "api_id" {
+  value = module.APIGateway.api_id
+}
