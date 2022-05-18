bdd634d7cb1043f2e0a75d2dae3e5061cc7aec79
Tapinder Singh <9sin063>
2022-05-18 23:51:10 +0530

    updated tf code

--- terraform/application/modules/api_gateway/main.tf
+++ terraform/application/modules/api_gateway/main.tf
@@ -28,9 +28,11 @@ resource "aws_api_gateway_stage" "api_stage" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.deploy_api.id
 
+/*
   variables = {
     deployed_at = timestamp()
   }
+  */
 
   tags = merge(
     var.common_tags,
